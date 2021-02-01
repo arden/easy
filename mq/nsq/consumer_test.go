6223f866fea2e64bcf9a5b5e226623ef1242a7d0
arden <arden@tujiao.com>
2021-02-01 15:56:49 +0800

    fix

--- mq/nsq/consumer_test.go
+++ mq/nsq/consumer_test.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"github.com/arden/easy/core/logger"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 )
@@ -28,4 +31,31 @@ func TestNsqConsumer_AddHandler(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestNsqConsumer_RunHandler(t *testing.T) {
+	consumer := &NsqHandler{
+		Topic:            "test",
+		Channel:          "ai",
+		OpenChannelTopic: true,
+	}
+
+	consumer.SetHandle(func(log *logger.Logger, message *nsq.Message) error {
+		assert.Equal(t, []byte(`hello`), message.Body)
+		fmt.Println("====>", string(message.Body))
+		return nil
+	})
+
+	Register("test", consumer)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	p, err := NewProducer("default")
+	require.NoError(t, err)
+	err = p.Publish("test", []byte(`hello`))
+	require.NoError(t, err)
+	go func() {
+		time.Sleep(3 * time.Second)
+		cancel()
+	}()
+	Run("test", ctx)
 }
